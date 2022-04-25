585410db3d21a1b9c027ab671393b950cb56595f
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-04-25 21:45:07 +0800

    update deploy script

--- docs/deploy.ps1
+++ docs/deploy.ps1
@@ -10,10 +10,6 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git --bare init
-git add -A
-git commit -m 'deploy'
-
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
