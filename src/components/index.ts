6887df7d2335e386d5936d19357dc3e9b7d5176d
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-05-03 23:17:06 +0800

    added docs

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import { utcToTime, utcToDate, convertTimeZone, getWeather } from './utils'
 import VueOpenWeather from './VueOpenWeather.vue'
 import VueCurrentWeather from './VueCurrentWeather.vue'
 
+/* A way to register the component globally. */
 export const OpenWeather = {
   install: (app: App) => {
     app.component('VueOpenWeather', VueOpenWeather)
--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,5 +1,10 @@
-// Function to account for different Time Zone based on offset
-// given UTC dt string from OpenWeather
+/**
+ * It takes a time in seconds and an offset in seconds and returns a new Date object with the timezone
+ * offset applied
+ * @param {number} time - the time in seconds since the epoch
+ * @param {number} offset - The offset of the timezone you want to convert to.
+ * @returns A Date object.
+ */
 export const convertTimeZone = (time: number, offset: number) => {
   const date = new Date((time + offset) * 1000)
   const utc = date.getTime() + (date.getTimezoneOffset() * 60000)
@@ -7,7 +12,12 @@ export const convertTimeZone = (time: number, offset: number) => {
   return nd
 }
 
-// Function to convert UTC string to date with day of week
+/**
+ * It takes a UTC time and an offset and returns a date in the format of "Day, Day of Month Month"
+ * @param {number} time - number - the time in milliseconds
+ * @param {number} offset - -28800
+ * @returns A string in the format of "Monday, 1 January"
+ */
 export const utcToDate = (time: number, offset: number) => {
   const date = convertTimeZone(time, offset)
   const day = date.toLocaleString('default', { weekday: 'long' })
@@ -16,7 +26,12 @@ export const utcToDate = (time: number, offset: number) => {
   return `${day}, ${dayOfMonth} ${month}`
 }
 
-// Function to convert UTC to time in 12 hour format
+/**
+ * It takes a UTC time and an offset and returns a formatted time
+ * @param {number} time - number - the time in milliseconds
+ * @param {number} offset - -28800
+ * @returns A string in the format of "HH:MM am/pm"
+ */
 export const utcToTime = (time: number, offset: number) => {
   const date = convertTimeZone(time, offset)
   const hours = date.getHours()
@@ -28,11 +43,32 @@ export const utcToTime = (time: number, offset: number) => {
   return `${formattedHour}:${formattedMinutes} ${ampm}`
 }
 
+/**
+ * It takes an array of strings and returns a string of comma separated values
+ * @param {any[]} excludes - any[]
+ * @returns A function that takes an array of any type and returns a string of the array elements
+ * joined by a comma.
+ */
 export const excludeString = (excludes: any[]) => {
   return excludes.join(',')
 }
 
-// Function that returns OpenWeatherAPI call
+/**
+ * It takes an API key, latitude, longitude, units, and an array of strings that represent the data you
+ * want to exclude from the response. 
+ * 
+ * It returns a promise that resolves to an object containing the weather data. 
+ * 
+ * The function uses the fetch API to make a request to the OpenWeatherMap API. 
+ * 
+ * @param {string} apiKey - string - Your OpenWeatherMap API key
+ * @param {string} lat - string,
+ * @param {string} lon - longitude
+ * @param [units=metric] - metric, imperial, or standard
+ * @param excludes - An array of strings that are the names of the data points you want to exclude from
+ * the response.
+ * @returns An object with the weather data.
+ */
 export const getWeather = async (
   apiKey: string,
   lat: string,
